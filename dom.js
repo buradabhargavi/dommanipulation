da4cf2125aecb99694fef2bf68bd1ddf6d022c62
buradabhargavi <bhargaviboorada246@gmail.com>
2023-04-08 20:27:27 +0530

    funny1 commit

---
+++ dom.js
@@ -0,0 +1,31 @@
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//console.log(document.title);
+document.title= "item page";
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textcontent ='Hello';
+//console.log(document.forms);
+//console.log(document.links);
+//console.log(document.forms[0]);
+//console.log(document.images);
+
+
+//GETELEMENTBYID//
+//console.log(document.getElementById('header-title'));
+var headerTitle = document.getElementById('header-title');
+//var header= document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textcontent='Hello';
+//headerTitle.innerText='Goodbye';
+//console.log(headerTitle.innertext);
+//headerTitle.innerHTML = '<h3>Hello</>';
+headerTitle.style.borderBottom = 'solid 3px #000'
+//header.style.borderBottom = 'solid 3px #000'
+
+headerTitle.style.fontWeight = 'bold';
+headerTitle.style.color='green';
